f8292228fe7dcd08c3916245fbf7010aa4ab990b
AnshP05 <23parasharansh@gmaill.com>
2025-07-21 16:07:19 -0700

    Deleted unnessecary files from the transfer from codesandbox to vscode as well as added the manifest.json to the root for the chrome extension.

--- src/index.mjs
+++ src/index.mjs
@@ -1,13 +1,32 @@
 
 
 let myLeads = [];
-const inputEl = document.getElementById("input-el")
+let inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value)
-  console.log(myLeads)
+  renderLeads()
+  inputEl.value = ""
 
 });
+
+function renderLeads() {
+  let listItems = ""
+  for (let i = 0; i < myLeads.length; i++) {
+    let url = myLeads[i]
+    if (!url.startsWith("http")) {
+      url = "https://" + url
+    }
+    listItems += `
+            <li>
+              <a target='_blank' href='${url}'>
+                ${myLeads[i]}
+              </a>
+            </li>
+    `
+  }
+  ulEl.innerHTML = listItems
+}
 
