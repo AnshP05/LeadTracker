7dacc97b633ec8fd1d8dc1006a2da66ecf471b86
AnshP05 <23parasharansh@gmaill.com>
2025-07-21 17:11:48 -0700

    delete button now works. When double clicked clears the local storage and re renders so that there are no links that show up in memory or on display

--- src/index.mjs
+++ src/index.mjs
@@ -4,34 +4,45 @@ let myLeads = [];
 let inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
 
-localStorage
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
-inputBtn.addEventListener("click", function () {
-  myLeads.push(inputEl.value)
-  inputEl.value = ""
-  renderLeads()
-
-  localStorage.setItem("myLeads", JSON.stringify(myLeads))
-
-
-});
-
-function renderLeads() {
+if(leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage
+  render(myLeads)
+}
+function render(leads) {
   let listItems = ""
-  for (let i = 0; i < myLeads.length; i++) {
-    let url = myLeads[i]
+  for (let i = 0; i < leads.length; i++) {
+    let url = leads[i]
     if (!url.startsWith("http")) {
       url = "https://" + url
     }
     listItems += `
             <li>
               <a target='_blank' href='${url}'>
-                ${myLeads[i]}
+                ${leads[i]}
               </a>
             </li>
     `
   }
   ulEl.innerHTML = listItems
 }
+
+deleteBtn.addEventListener("dblclick", function() {
+  localStorage.clear()
+  myLeads = []
+  render(myLeads)
+})
+inputBtn.addEventListener("click", function () {
+  myLeads.push(inputEl.value)
+  inputEl.value = ""
+  render(myLeads)
+
+  localStorage.setItem("myLeads", JSON.stringify(myLeads))
+
+
+})
+
 
